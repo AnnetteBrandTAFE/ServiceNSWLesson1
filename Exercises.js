e6c998ad91befd8dad92d73e8bfd234156554d0e
Annette Brand <Annette.Brand1@studytafensw.edu.au>
2020-05-19 19:52:27 +1000

    Revision

---
+++ Exercises.js
@@ -0,0 +1,33 @@
+// Exercises
+
+// Q1 Create a variable called myPet and assign it a string value "Dog"
+
+let myPet = "Dog";
+
+// Q2 Create a variable called petName and assign it a string value "Rex"
+
+petName = "Rex";
+
+// Q3 Create a variable called petAge and assign it a number value 6
+
+petAge = 6;
+
+// Q4 Use string concatenation to print to the console so the output is 
+//"I have a Dog called Rex who is 6 years old".
+
+console.log("I have a " + myPet + " called " + petName + " who is " + petAge + " years old.");
+
+// Q5 Use string interpolation to print the same output to the console
+
+console.log(`I have a ${myPet} called ${petName} who is ${petAge} years old.`);
+
+// Q6 Update petAge by using the Addition operator to increase its value by 2
+
+console.log(petAge);
+petAge = 6+2;
+console.log(petAge);
+
+// Q6 Call the same statement from question 4 or 5 again after updating the petAge
+
+console.log("I have a " + myPet + " called " + petName + " who is " + petAge + " years old.");
+console.log(`I have a ${myPet} called ${petName} who is ${petAge} years old.`);
